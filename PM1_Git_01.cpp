9651fa50de82ebca7a1adfff7b8f3a3aa6f4c445
pepe <kousaku5963@gmail.com>
2022-11-09 11:13:10 +0900

    ローカルでコンフリクト変更

--- PM1_Git_01.cpp
+++ PM1_Git_01.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello Visual Studio Git!\n";
-    std::cout << "New branch changed\n";
+    std::cout << "Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
